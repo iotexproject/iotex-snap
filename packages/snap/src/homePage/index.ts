9a612b0525e007ac44b1a26dd38b7267f27950c6
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2024-01-25 19:00:50 +0300

    Update index.ts

--- packages/snap/src/homePage/index.ts
+++ packages/snap/src/homePage/index.ts
@@ -4,8 +4,11 @@ import { panel, text, heading } from '@metamask/snaps-sdk';
 export const onHomePage: OnHomePageHandler = async () => {
   return {
     content: panel([
-      heading('Hello world!'),
-      text('Welcome to my Snap home page!'),
+      heading('IoTeX snap'),
+      heading('Following features are supported:'),
+      text('Address conversion from 0x to io address'),
+      text('Address conversion from io to 0x address'),
+      text('Sending assets to INS domain in on IoTeX mainnet'),
     ]),
   };
 };
