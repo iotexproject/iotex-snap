472b3e534a21e0151c8cb2444e991e14ca58b1f2
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2024-01-25 18:36:45 +0300

    Update Header.tsx

--- packages/site/src/components/Header.tsx
+++ packages/site/src/components/Header.tsx
@@ -64,7 +64,7 @@ export const Header = ({
     <HeaderWrapper>
       <LogoWrapper>
         <SnapLogo color={theme.colors.icon?.default} size={36} />
-        <Title>template-snap</Title>
+        <Title>iotex-snap</Title>
       </LogoWrapper>
       <RightContainer>
         <Toggle
