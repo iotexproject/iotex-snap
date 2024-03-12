ed123d7a2ce621c2474b78f81c91e8ce692ea8eb
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2024-03-12 23:06:20 +0300

    update home page

--- packages/snap/src/homePage/index.ts
+++ packages/snap/src/homePage/index.ts
@@ -7,8 +7,6 @@ export const onHomePage: OnHomePageHandler = async () => {
       heading('IoTeX snap'),
       heading('Following features are supported:'),
       text('Address conversion from 0x to io address'),
-      text('Address conversion from io to 0x address'),
-      text('Sending assets to INS domain in on IoTeX mainnet'),
     ]),
   };
 };
